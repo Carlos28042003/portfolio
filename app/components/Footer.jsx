67b13f4ea39f4b3b78270894cf96f897228961aa
Carlos28042003 <162278282+Carlos28042003@users.noreply.github.com>
2025-02-27 21:01:27 +0530

    Update Footer.jsx

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -13,7 +13,7 @@ const Footer = (isDarkMode) => {
                 </div>
             </div>
             <div className='text-center sm:flex items-center justify-between border-t border-gray-400 mx-[10%] mt-12 py-6'>
-                <p>© 2025 Carlos Cherindza. All rights reserved.</p>
+                <p>&copy;{new Date().getFullYear()} Carlos Cherindza. All rights reserved.</p>
                 <ul className="flex itms-center gap-10 justify-center mt-4 sm:mt-0">
                     <li><a target="_blank" rel="noopener noreferrer" href="https://github.com/Carlos28042003">
                             GitHub</a>
