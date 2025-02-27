095f0633588ab46f291d47e0ac7b53261f5ee77e
Carlos28042003 <162278282+Carlos28042003@users.noreply.github.com>
2025-02-27 21:16:18 +0530

    Update assets.js

--- assets/assets.js
+++ assets/assets.js
@@ -35,7 +35,7 @@ import right_arrow from './right-arrow.png';
 import send_icon from './send-icon.png';
 import right_arrow_bold from './right-arrow-bold.png';
 import right_arrow_bold_dark from './right-arrow-bold-dark.png';
-import photoshop from 'photoshop.png'
+import photoshop from './photoshop.png';
 export const assets = {
     user_image,
     code_icon,
